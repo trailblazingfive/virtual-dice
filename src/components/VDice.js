a08d92fcf91b14d040c2d6c82df96cdc2e92e830
tbf <contact@trailblazingfive.net>
2021-04-13 10:57:10 +0200

    added roll details

--- src/components/VDice.js
+++ src/components/VDice.js
@@ -15,13 +15,28 @@ const Title = styled.h1`
   padding-bottom: 5px;
   font-size: 2em;
 `
-const RollResult = styled.h1`
+// const RollResult = styled.h1`
+//   margin-top: 10px;
+//   text-align: center;
+//   line-height: 50px;
+//   padding-top: 5px;
+//   padding-bottom: 5px;
+//   font-size: 2em;
+// `
+
+const RollResult = styled.div`
   margin-top: 10px;
   text-align: center;
   line-height: 50px;
   padding-top: 5px;
   padding-bottom: 5px;
   font-size: 2em;
+`
+
+const ModContainer = styled.div`
+  display: flex;
+  flex-direction: rows;
+  vertical-align: middle;
 `
 
 const VDice = (props) => {
@@ -39,11 +54,35 @@ const VDice = (props) => {
 
   const rollDice = (dSide, dNumber, rollMod) => {
     let result = 0
+    let rolls = []
+    let rollModStr = ''
     for (let i = 1; i <= dNumber; i++) {
-      result += getRandomIntInclusive(1, dSide)
+      rolls.push(getRandomIntInclusive(1, dSide))
+      result += rolls[rolls.length - 1]
     }
     result += rollMod
-    setRollResult(result)
+    console.log(rolls)
+    if (rollMod === 0) {
+      rollModStr = ""
+    } else if (rollMod > 0) {
+      rollModStr = `+${rollMod}`
+    } else if (rollMod < 0) {
+      rollModStr = `${rollMod}`
+    }
+    setRollResult({
+      total: result,
+      rolls: rolls,
+      rollsStr: arrToStr(rolls),
+      diceTypeStr: `${dNumber}d${dSide}${rollModStr}`
+    })
+  }
+
+  const arrToStr = (arr) => {
+    let str = ''
+    for (let i = 0; i < arr.length; i++) {
+      str += `${arr[i]},`
+    }
+    return str.slice(0, -1)
   }
 
   const rollSettings = {
@@ -55,6 +94,11 @@ const VDice = (props) => {
   return (
     <div className="VDice">
       <Title>Virtual Dice</Title>
+      <RollResult>
+        <p>{rollResult.diceTypeStr}</p>
+        <p>{rollResult.total}</p>
+        <p>{rollResult.rollsStr}</p>
+      </RollResult>
       <SingleDice dSide={4} rollSettings={rollSettings}></SingleDice>
       <SingleDice dSide={6} rollSettings={rollSettings}></SingleDice>
       <SingleDice dSide={8} rollSettings={rollSettings}></SingleDice>
@@ -62,9 +106,10 @@ const VDice = (props) => {
       <SingleDice dSide={12} rollSettings={rollSettings}></SingleDice>
       <SingleDice dSide={20} rollSettings={rollSettings}></SingleDice>
       <SingleDice dSide={100} rollSettings={rollSettings}></SingleDice>
-      <ModButton name={'Set number of dice'} value={dNumber} cbValue={setDNumber}></ModButton>
-      <ModButton name={'Set roll modification'} value={rollMod} cbValue={setRollMod}></ModButton>
-      <RollResult>{rollResult}</RollResult>
+      <ModContainer>
+        <ModButton name={'Set number of dice'} value={dNumber} cbValue={setDNumber}></ModButton>
+        <ModButton name={'Set roll modification'} value={rollMod} cbValue={setRollMod}></ModButton>
+      </ModContainer>
     </div>
   )
 
