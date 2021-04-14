9b72c170fc640bb76bfb3683ddb82320b1619269
tbf <contact@trailblazingfive.net>
2021-04-15 01:35:46 +0200

    new components

---
+++ src/components/Backdrop.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './css/Backdrop.css'
+
+const Backdrop = (props) => {
+  return (
+    <div  className="backdrop image"></div>    
+  )
+}
+
+export default Backdrop
---
+++ src/components/Credit.js
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import styled from 'styled-components';
+
+const StyledLink = styled.a`
+  font-size: 0.5em;
+  text-align: right;
+  text-decoration: none;
+  color: rgba(255, 255, 255, 0.5);
+  :hover {
+    color: rgba(255,255,255, 0.95);
+  }
+`
+
+const StyledP = styled.p`
+  font-size: 0.5em;
+  text-align: right;
+  text-decoration: none;
+  color: rgba(255, 255, 255, 0.5);
+  display: inline;
+`
+
+const CreditDiv = styled.div`
+  position: fixed;
+  bottom: 0.5vw;
+  right: 0.5vw;
+`
+
+const Credit = () => {
+
+  return (
+    <CreditDiv>
+      <StyledLink
+        href="https://www.flickr.com/photos/nattydread011/50759970901/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        background art by Natty Dread
+    </StyledLink>
+    <StyledP> | </StyledP>
+      <StyledLink
+        href="https://github.com/trailblazingfive"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        by trailblazingfive 2021
+    </StyledLink>
+    </CreditDiv>
+  );
+};
+
+export default Credit;
