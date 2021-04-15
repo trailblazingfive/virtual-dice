6ef68c5564f4209416c64b6db012b1a5ae25f769
tbf <contact@trailblazingfive.net>
2021-04-15 15:58:22 +0200

    bounded mod buttons

--- src/components/ModButton.js
+++ src/components/ModButton.js
@@ -5,14 +5,29 @@ const ModButton = (props) => {
 
   const [value, setValue] = useState(props.value)
 
-  let incValue = () => {
-    setValue(value + 1)
-    props.cbValue(value + 1)
+  let incValue = (min) => {
+    if (min === false) {
+      setValue(value + 1)
+      props.cbValue(value + 1)
+    } else {
+      if ((value+1) >= min) {
+        setValue(value + 1)
+        props.cbValue(value + 1)
+      }
+    }
+
   }
 
-  let decValue = () => {
-    setValue(value - 1)
-    props.cbValue(value - 1)
+  let decValue = (min) => {
+    if (min === false) {
+      setValue(value - 1)
+      props.cbValue(value - 1)
+    } else {
+      if ((value-1) >= min) {
+        setValue(value - 1)
+        props.cbValue(value - 1)
+      }
+    }
   }
 
 
@@ -20,9 +35,9 @@ const ModButton = (props) => {
     <div className="ModButton">
       <h1>{props.name}</h1>
       <div className="Controls">
-        <button onClick={() => decValue()}>-</button>
+        <button onClick={() => decValue(props.min)}>-</button>
         <div>{value}</div>
-        <button onClick={() => incValue()}>+</button>
+        <button onClick={() => incValue(props.min)}>+</button>
       </div>
     </div>
   )
