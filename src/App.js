23eb2499d08a13c07e4c55b8566bd4098a8db3f5
tbf <contact@trailblazingfive.net>
2021-04-17 21:45:19 +0200

    imported view select to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import VDice from './components/VDice'
+import SelectView from './components/SelectView'
 
 function App() {
   return (
     <div className="App">
+      {/* <SelectView></SelectView> */}
       <VDice></VDice>
     </div>
   );
